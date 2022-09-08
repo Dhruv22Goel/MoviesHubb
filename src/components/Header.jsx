2e32e7822edf37c001b52f2d63bbdce2b97ce4b9
Dhruv22Goel <91088068+Dhruv22Goel@users.noreply.github.com>
2022-09-08 17:10:06 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,29 @@
 import styled from "styled-components";
 const HeaderDiv =styled.div`
-    width: 100%;
-    height:10vh;
-    display: flex;
-    justify-content: center;
-    font-family: "Montserrat", sans-serif;
-    font-size: 60px;
-    padding-bottom: 15px;
-    box-shadow: 0px 1px 5px black;
-    color: white;
-    z-index: 100;
+  width: 100%;
+  height:10vh;
+  display: flex;
+  justify-content: center;
+  font-family: "Montserrat", sans-serif;
+  font-size: 60px;
+  padding-bottom: 15px;
+  box-shadow: 0px 1px 5px black;
+  color: white;
+  z-index: 100;
 `
-  
-const Header = () => {
+const Img = styled.img`
+  margin-left: 22vw;
+  margin-right:0;
+  margin-top:15px;
+`
+const Div = styled.div`
+  margin-left:25vw;
+`
+const Header = (props) => {
   return (
-    <HeaderDiv onClick={() => window.scroll(0, 0)}>
-      🎬 MOVIES HUBB 🎥
-    </HeaderDiv>
+  <HeaderDiv onClick={() => window.scroll(0, 0)}>
+    <Div>🎬 MOVIES HUBB 🎥</Div> <Img src={props.img}/>
+  </HeaderDiv>
   );
 };
 
