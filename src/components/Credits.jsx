c9b0c71fff189df2e75199ab9fd89dd9dacfc09b
Dhruv22Goel <91088068+Dhruv22Goel@users.noreply.github.com>
2022-09-08 17:08:58 +0530

    Update Credits.jsx

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -3,8 +3,6 @@ import styled from "styled-components"
 import { Link } from "react-router-dom"
 import FooterBG from "../footer-bg.jpg";
 const CreditsDiv = styled.div`
-    height:10vh;
-    max-height:10vh;
     color:white;
     font-family:sans-serif;
     margin-top:100px;
@@ -17,25 +15,36 @@ const Text = styled.div`
     height:fit-content;
     width:fit-content;
 `
-const Redirects=styled.div`
+const Redirects = styled.div`
     display:flex;
     flex-direction:row;
     padding:10px;
 `
-export default function Credits(){
-    function scroll(){
+const Alpha = styled.div`
+    display:flex;
+    flex-direction:column;
+    gap:10px;
+`
+export default function Credits() {
+    function scroll() {
         window.scrollTo(0, 0);
     }
-    return(
+    return (
         <CreditsDiv>
             <Redirects>
                 <Link to="/" className="link link3" exact onClick={scroll}>Home</Link>
                 <Link to="/about" className="link link3">About Us</Link>
-                
-                <Link to="/toprated" className="link link3">Top Rated</Link>
-                <Link to="/search" className="link link3">Search</Link>
+                <Alpha>
+                    <Link to="/toprated" className="link link3">Top Rated</Link>
+                    <Link to="/upcoming" className="link link3">Upcoming</Link>
+                </Alpha>
+                <Alpha>
+                    <Link to="/search" className="link link3">Search</Link>
+                    <Link to="/discover" className="link link3">Discover</Link>
+                </Alpha>
+                    {/* <Link to="/login" className="link link3">Login</Link> */}
             </Redirects>
-            <Text>© Dhruv Goel 2022</Text>
+            <Text>© 2022</Text>
         </CreditsDiv>
     )
 }
