3889912ce295055143c7538094d231d9d98c0584
Dhruv22Goel <91088068+Dhruv22Goel@users.noreply.github.com>
2022-09-17 09:38:12 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import NavBar from "../components/NavBar";
 import ProfilePic from "../ProfilePic (2).jpg";
 import KanavProfilePic from "../WhatsApp Image 2022-09-09 at 2.11.36 PM.jpeg";
+import PushkarProfilePic from "../";
 
 const AboutDiv = styled.div`
     max-height:100vh;
@@ -70,6 +71,18 @@ export default function About(){
                     <h3>KANAV KAPOOR</h3>
                     <p><b>Student, Web Developer, and Software Engineer in Delhi,India</b></p>
                     <P>Hey there, I’m Kanav. I’m a student living in IF/140 Lajpat Nagar -1 New Delhi-110024 . I am a fan of technology, music, and acting. I’m also interested in camping and running. You can hire me with a click on the button above.</P>
+                </AboutMeDiv>
+            </AboutMe>
+        </AboutDiv>
+        <AboutDiv>
+            <AboutMe>
+                <AboutMeImgDiv>
+                    <AboutMeImg src={PushkarProfilePic} />
+                </AboutMeImgDiv>
+                <AboutMeDiv>
+                    <h3>PUSHKAR KUMAR</h3>
+                    <p><b>Student, Web Developer, and Software Engineer in Delhi,India</b></p>
+                    <P>Hey, I’m Pushkar. I’m a student living in Delhi, India. I am a fan of technology, programming, and coffee. You can hire me with a click on the button above.</P>
                 </AboutMeDiv>
             </AboutMe>
         </AboutDiv>
